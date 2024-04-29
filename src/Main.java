118ffbbfbcf7b86e7dec35cb1bff933d0f66b3c5
siniwyh <siniwyh@icloud.com>
2024-04-29 19:48:50 +0700

    code added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 21121;
+        int mileFor = 20;
+        int miles = cost / mileFor;
+        System.out.println("Количество начисляемых миль: " + miles);
+    }
+}
